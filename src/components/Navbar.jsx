12c4562c7fca630ecba70516853bb48b207ed5b1
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-06-20 09:27:16 -0700

    Add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar gap-2">
+      <div className="flex-none">
+        <button className="btn btn-square btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        </button>
+      </div>
+
+      <div className="flex-1 gap-2">
+        <button className="btn btn-primary normal-case text-xl">About</button>
+        <button className="btn btn-primary normal-case text-xl">Portfolio</button>
+        <button className="btn btn-primary normal-case text-xl">Contact</button>
+      </div>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    require("daisyui"),
   ],
   daisyui: {
-    themes: ['lofi'],
+    themes: ["lofi"],
   },
 }
