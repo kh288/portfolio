498d08b55a7bc4656c8614865fb95142572168b6
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-06-20 08:43:29 -0700

    Fix tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <>
-    </>
+    <div className='m-3'>
+      <p>
+        Test
+      </p>
+    </div>
   );
 }
 
