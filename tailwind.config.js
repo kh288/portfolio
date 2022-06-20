c89c5adf6ce68e1313234be1c5d432ae2731bb7c
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-06-20 08:58:17 -0700

    Add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: [],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ['lofi'],
+  },
 }
