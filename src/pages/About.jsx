f7b20fc2d48a3192a261198f7939f035933040f5
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-06-20 11:14:06 -0700

    Add About Content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div>About</div>
+    <div className="card rounded shadow m-3">
+      <div className="card-body text-center">
+        <h2 className='card-title'>About</h2>
+        <h2>Hello, my name is Kevin Hernandez</h2>
+        <p>Fullstack Web Developer</p>
+        <p>I'm a recent graduate at UC Berkeley Extension in Web Development in MERN Stack</p>
+      </div>
+    </div>
   )
 }
